b1f01dacdc39d0f930f8a03e3aa7cb62882744be
Sunset <grigory.orlov.set@gmail.com>
2023-05-31 01:44:45 +0500

    Fartify 2.0
    Co-Authored-By: Aimarekin <aimarekin@gmail.com>

--- fartify.js
+++ fartify.js
@@ -1,9 +1,3 @@
-addEventListener('click', () => {
-  const fart = document.createElement('audio');
-  fart.src = 'https://www.myinstants.com/media/sounds/fart-with-reverb.mp3';
-  document.body.appendChild(fart);
-  fart.play();
-  fart.addEventListener('ended', () => {
-    fart.parentNode.removeChild(fart);
-  });
-});
+const fart = document.createElement("audio");
+fart.src = "https://www.myinstants.com/media/sounds/fart-with-reverb.mp3";
+addEventListener("click", () => fart.play());
