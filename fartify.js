12ca458f81bb6efbb56bddbba2591a391db8ae93
Sunset <grigory.orlov.set@gmail.com>
2023-05-31 02:59:45 +0500

    Fartify 3.0

--- fartify.js
+++ fartify.js
@@ -1,3 +1,9 @@
 const fart = document.createElement("audio");
 fart.src = "https://www.myinstants.com/media/sounds/fart-with-reverb.mp3";
-addEventListener("click", () => fart.play());
+addEventListener("mousedown", () => {
+  const currentFart = fart.cloneNode();
+  currentFart.play();
+  currentFart.addEventListener("ended", () => {
+    currentFart.remove()
+  });
+});
